15851d26f23e9920e5614ef346dceffc2b7ea894
evan-walter <ewpw5678@gmail.com>
2024-12-29 01:35:58 -0500

    feat: install and config blink.cmp for completion

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -4,6 +4,7 @@ return {
     {
         'neovim/nvim-lspconfig',
         dependencies = {
+            'saghen/blink.cmp',
             {
                 "folke/lazydev.nvim",
                 ft = "lua", -- only load on lua files
@@ -17,7 +18,8 @@ return {
             },
         },
         config = function()
-            require('lspconfig').lua_ls.setup {}
+            local capabilities = require('blink.cmp').get_lsp_capabilities()
+            require('lspconfig').lua_ls.setup { capabilities = capabilities }
         end,
     }
 }
