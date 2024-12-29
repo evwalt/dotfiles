c32267b6ce76248cbda2e48944ebe58e1fc9dc66
evan-walter <ewpw5678@gmail.com>
2024-12-28 21:41:05 -0500

    chore: autoformat hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,7 +2,7 @@
 --
 function reloadConfig(files)
     doReload = false
-    for _,file in pairs(files) do
+    for _, file in pairs(files) do
         if file:sub(-4) == ".lua" then
             doReload = true
         end
@@ -11,13 +11,14 @@ function reloadConfig(files)
         hs.reload()
     end
 end
+
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
 print(os.getenv("HOME") .. "/.hammerspoon/")
 
 -- Application Focus
 --
-local hyper = {"ctrl", "alt", "cmd", "shift"}
+local hyper = { "ctrl", "alt", "cmd", "shift" }
 -- crl tns wvz fdb
 hs.hotkey.bind(hyper, "C", function()
     hs.application.launchOrFocus("Google Chrome")
@@ -61,11 +62,11 @@ end)
 
 -- Window Management
 --
--- lmr  
+-- lmr
 -- halves: left half: o; middle half: e/t; right half: n
 -- thirds: left third: j; middle third: k/m; right third: w
 -- fourths: ,rqv
-local ca = {"ctrl", "alt"}
+local ca = { "ctrl", "alt" }
 -- Helper function to move and resize windows
 local function moveAndResize(xRatio, yRatio, wRatio, hRatio)
     local win = hs.window.focusedWindow()
@@ -79,55 +80,55 @@ local function moveAndResize(xRatio, yRatio, wRatio, hRatio)
     f.h = max.h * hRatio
     win:setFrameInScreenBounds(f, 0)
 end
-hs.hotkey.bind(ca, "return", function() moveAndResize(0, 0, 1, 1) end)  -- Maximized
-hs.hotkey.bind({"ctrl", "alt", "shift"}, "return", function() moveAndResize(0.05, 0.05, 0.9, 0.9) end)  -- Almost maximized
+hs.hotkey.bind(ca, "return", function() moveAndResize(0, 0, 1, 1) end)                                 -- Maximized
+hs.hotkey.bind({ "ctrl", "alt", "shift" }, "return", function() moveAndResize(0.05, 0.05, 0.9, 0.9) end) -- Almost maximized
 
 -- Vertical Halves
 --
-hs.hotkey.bind(ca, "o", function() moveAndResize(0, 0, 0.5, 1) end)  -- Left half
-hs.hotkey.bind(ca, "e", function() moveAndResize(0.25, 0, 0.5, 1) end)  -- Center half
-hs.hotkey.bind(ca, "t", function() moveAndResize(0.25, 0, 0.5, 1) end)  -- Center half (same as "e")
+hs.hotkey.bind(ca, "o", function() moveAndResize(0, 0, 0.5, 1) end)    -- Left half
+hs.hotkey.bind(ca, "e", function() moveAndResize(0.25, 0, 0.5, 1) end) -- Center half
+hs.hotkey.bind(ca, "t", function() moveAndResize(0.25, 0, 0.5, 1) end) -- Center half (same as "e")
 hs.hotkey.bind(ca, "n", function() moveAndResize(0.5, 0, 0.5, 1) end)  -- Right half
 
 -- Horisontal Halves
 --
-hs.hotkey.bind(ca, "y", function() moveAndResize(0, 0, 1, 0.5) end)  -- Top half
-hs.hotkey.bind(ca, "f", function() moveAndResize(0.5, 0.5, 1, 0.5) end)  -- Bottom half
+hs.hotkey.bind(ca, "y", function() moveAndResize(0, 0, 1, 0.5) end)     -- Top half
+hs.hotkey.bind(ca, "f", function() moveAndResize(0.5, 0.5, 1, 0.5) end) -- Bottom half
 
 -- Fourths - Corners
 --
-hs.hotkey.bind(ca, ",", function() moveAndResize(0, 0, 0.5, 0.5) end)  -- Top left
-hs.hotkey.bind(ca, "r", function() moveAndResize(0.5, 0, 0.5, 0.5) end)  -- Top right
-hs.hotkey.bind(ca, "v", function() moveAndResize(0.5, 0.5, 0.5, 0.5) end)  -- Bottom right
-hs.hotkey.bind(ca, "q", function() moveAndResize(0, 0.5, 0.5, 0.5) end)  -- Bottom left
+hs.hotkey.bind(ca, ",", function() moveAndResize(0, 0, 0.5, 0.5) end)     -- Top left
+hs.hotkey.bind(ca, "r", function() moveAndResize(0.5, 0, 0.5, 0.5) end)   -- Top right
+hs.hotkey.bind(ca, "v", function() moveAndResize(0.5, 0.5, 0.5, 0.5) end) -- Bottom right
+hs.hotkey.bind(ca, "q", function() moveAndResize(0, 0.5, 0.5, 0.5) end)   -- Bottom left
 
 -- Fourths - Centered
 --
-hs.hotkey.bind(ca, ".", function() moveAndResize(0.25, 0, 0.5, 0.5) end)  -- Top middle fourth
-hs.hotkey.bind(ca, "c", function() moveAndResize(0.25, 0, 0.5, 0.5) end)  -- Top middle fourth (same as ".")
-hs.hotkey.bind(ca, "p", function() moveAndResize(0.25, 0.25, 0.5, 0.5) end)  -- Center fourth
-hs.hotkey.bind(ca, "g", function() moveAndResize(0.25, 0.25, 0.5, 0.5) end)  -- Center fourth (same as "p")
+hs.hotkey.bind(ca, ".", function() moveAndResize(0.25, 0, 0.5, 0.5) end)    -- Top middle fourth
+hs.hotkey.bind(ca, "c", function() moveAndResize(0.25, 0, 0.5, 0.5) end)    -- Top middle fourth (same as ".")
+hs.hotkey.bind(ca, "p", function() moveAndResize(0.25, 0.25, 0.5, 0.5) end) -- Center fourth
+hs.hotkey.bind(ca, "g", function() moveAndResize(0.25, 0.25, 0.5, 0.5) end) -- Center fourth (same as "p")
 
 -- Vertical Thirds
 --
-hs.hotkey.bind(ca, "j", function() moveAndResize(0, 0, 1/3, 1) end)  -- Left third
-hs.hotkey.bind(ca, "k", function() moveAndResize(1/3, 0, 1/3, 1) end)  -- Middle third
-hs.hotkey.bind(ca, "m", function() moveAndResize(1/3, 0, 1/3, 1) end)  -- Middle third (same as "k")
-hs.hotkey.bind(ca, "w", function() moveAndResize(2/3, 0, 1/3, 1) end)  -- Right third
+hs.hotkey.bind(ca, "j", function() moveAndResize(0, 0, 1 / 3, 1) end) -- Left third
+hs.hotkey.bind(ca, "k", function() moveAndResize(1 / 3, 0, 1 / 3, 1) end) -- Middle third
+hs.hotkey.bind(ca, "m", function() moveAndResize(1 / 3, 0, 1 / 3, 1) end) -- Middle third (same as "k")
+hs.hotkey.bind(ca, "w", function() moveAndResize(2 / 3, 0, 1 / 3, 1) end) -- Right third
 
 -- Vertical Two-thirds
 --
-hs.hotkey.bind(ca, "x", function() moveAndResize(0, 0, 2/3, 1) end)  -- Left two-thirds
-hs.hotkey.bind(ca, "i", function() moveAndResize(1/6, 0, 2/3, 1) end)  -- Middle two-thirds
-hs.hotkey.bind(ca, "d", function() moveAndResize(1/6, 0, 2/3, 1) end)  -- Middle two-thirds (same as "i")
-hs.hotkey.bind(ca, "b", function() moveAndResize(1/3, 0, 2/3, 1) end)  -- Right two-thirds
+hs.hotkey.bind(ca, "x", function() moveAndResize(0, 0, 2 / 3, 1) end) -- Left two-thirds
+hs.hotkey.bind(ca, "i", function() moveAndResize(1 / 6, 0, 2 / 3, 1) end) -- Middle two-thirds
+hs.hotkey.bind(ca, "d", function() moveAndResize(1 / 6, 0, 2 / 3, 1) end) -- Middle two-thirds (same as "i")
+hs.hotkey.bind(ca, "b", function() moveAndResize(1 / 3, 0, 2 / 3, 1) end) -- Right two-thirds
 
 -- Vertical Thirds with Horizontal Halves
 --
-hs.hotkey.bind(ca, "'", function() moveAndResize(0, 0, 1/3, 1/2) end)  -- Left upper third
-hs.hotkey.bind(ca, "a", function() moveAndResize(1/3, 0, 1/3, 1/2) end)  -- Middle upper third
-hs.hotkey.bind(ca, "s", function() moveAndResize(1/3, 1/2, 1/3, 1/2) end)  -- Middle lower third
-hs.hotkey.bind(ca, "l", function() moveAndResize(2/3, 0, 1/3, 1/2) end)  -- Right upper third
-hs.hotkey.bind(ca, ";", function() moveAndResize(0, 1/2, 1/3, 1/2) end)  -- Left lower third
-hs.hotkey.bind(ca, "z", function() moveAndResize(2/3, 1/2, 1/3, 1/2) end)  -- Right lower third
+hs.hotkey.bind(ca, "'", function() moveAndResize(0, 0, 1 / 3, 1 / 2) end) -- Left upper third
+hs.hotkey.bind(ca, "a", function() moveAndResize(1 / 3, 0, 1 / 3, 1 / 2) end) -- Middle upper third
+hs.hotkey.bind(ca, "s", function() moveAndResize(1 / 3, 1 / 2, 1 / 3, 1 / 2) end) -- Middle lower third
+hs.hotkey.bind(ca, "l", function() moveAndResize(2 / 3, 0, 1 / 3, 1 / 2) end) -- Right upper third
+hs.hotkey.bind(ca, ";", function() moveAndResize(0, 1 / 2, 1 / 3, 1 / 2) end) -- Left lower third
+hs.hotkey.bind(ca, "z", function() moveAndResize(2 / 3, 1 / 2, 1 / 3, 1 / 2) end) -- Right lower third
 
