9b2e90abdbca37b72a9964445fb08483c8f8c4c1
evan-walter <ewpw5678@gmail.com>
2025-01-26 12:58:19 -0800

    feat: add keymap to move to first or last in bufferline

--- .config/nvim/lua/config/plugins/bufferline.lua
+++ .config/nvim/lua/config/plugins/bufferline.lua
@@ -3,12 +3,19 @@ return {
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
 	config = function()
-		require("bufferline").setup({})
+		local bufferline = require("bufferline")
+		bufferline.setup({})
 
 		vim.keymap.set("n", "<space>j", ":BufferLineCyclePrev<CR>")
 		vim.keymap.set("n", "<space>k", ":BufferLineCycleNext<CR>")
 		vim.keymap.set("n", "<<", ":BufferLineMovePrev<CR>")
 		vim.keymap.set("n", ">>", ":BufferLineMoveNext<CR>")
+		vim.keymap.set("n", "<space><space>m0", function()
+			bufferline.move_to(1)
+		end)
+		vim.keymap.set("n", "<space><space>m$", function()
+			bufferline.move_to(-1)
+		end)
 		vim.keymap.set("n", "<space>pp", ":BufferLineGoToBuffer 1<CR>")
 		vim.keymap.set("n", "<space>gg", ":BufferLineGoToBuffer 1<CR>")
 		vim.keymap.set("n", "<space>..", ":BufferLineGoToBuffer 2<CR>")
