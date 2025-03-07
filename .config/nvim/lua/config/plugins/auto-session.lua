7bc650c7e25cbb0f524bf59b5cd51c2f90fba4e6
evan-walter <ewpw5678@gmail.com>
2025-03-07 14:02:21 -0800

    feat: widen auto-session picker

--- .config/nvim/lua/config/plugins/auto-session.lua
+++ .config/nvim/lua/config/plugins/auto-session.lua
@@ -10,6 +10,16 @@ return {
 		-- log_level = 'debug',
 		-- auto_restore_last_session = true,
 		show_auto_restore_notif = true,
+		session_lens = {
+			theme_conf = {
+				layout_config = {
+					-- As one example, can change width/height of picker
+					-- width = 0.8,    -- percent of window
+					width = { padding = 2 }, -- percent of window
+					-- height = 0.5,
+				},
+			},
+		},
 	},
 	keys = {
 		{ "<space>uus", ":SessionSearch<CR>" },
