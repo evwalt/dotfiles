2309c347d63a21185236b38ef0a34996dd3b98d0
evwalt <ewpw5678@gmail.com>
2025-03-09 18:58:48 -0700

    feat: change from mini hi to todo-comment

---
+++ .config/nvim/lua/config/plugins/todo-comments.lua
@@ -0,0 +1,16 @@
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		highlight = {
+			pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
+			comments_only = false, -- uses treesitter to match keywords in comments only
+		},
+		search = {
+			pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+		},
+	},
+}
