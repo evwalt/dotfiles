f00e03ce04a9b09a06127dbe01f59f0a51363234
evwalt <ewpw5678@gmail.com>
2025-04-09 21:02:15 -0700

    feat: add virtual text

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -94,3 +94,13 @@ vim.api.nvim_create_autocmd("BufEnter", {
 		end
 	end,
 })
+
+-- Virtual Text
+--
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+})
