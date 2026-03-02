bcf61f195645fad35f0759c10d7fc797a7befc14
evwalt <ewpw5678@gmail.com>
2026-03-02 09:06:15 -0800

    feat(nvim): add absolute path to lualine

--- .config/nvim/lua/config/plugins/lualine.lua
+++ .config/nvim/lua/config/plugins/lualine.lua
@@ -5,15 +5,68 @@ return {
 
 	config = function()
 		require("lualine").setup({
-			sections = {
-				lualine_x = {
-					{
-						require("noice").api.statusline.mode.get,
-						cond = require("noice").api.statusline.mode.has,
-						color = { fg = "#ff9e64" },
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				always_show_tabline = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+					refresh_time = 16, -- ~60fps
+					events = {
+						"WinEnter",
+						"BufEnter",
+						"BufWritePost",
+						"SessionLoadPost",
+						"FileChangedShellPost",
+						"VimResized",
+						"Filetype",
+						"CursorMoved",
+						"CursorMovedI",
+						"ModeChanged",
 					},
 				},
 			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				-- lualine_c = { "filename" },
+				lualine_c = {
+					{ "filename", path = 3 },
+				},
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				-- lualine_x = {
+				-- 	{
+				-- 		require("noice").api.statusline.mode.get,
+				-- 		cond = require("noice").api.statusline.mode.has,
+				-- 		color = { fg = "#ff9e64" },
+				-- 	},
+				-- },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = {},
 		})
 	end,
 }
