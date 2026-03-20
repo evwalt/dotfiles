d00550a401dff71fc28d8a2dc9906aac6d4b4db2
evwalt <ewpw5678@gmail.com>
2026-03-20 09:40:13 -0700

    feat(nvim): set notifications max width/height and timeout

--- .config/nvim/lua/config/plugins/nvim-notify.lua
+++ .config/nvim/lua/config/plugins/nvim-notify.lua
@@ -1,7 +1,21 @@
+-- return {
+-- 	"rcarriga/nvim-notify",
+-- 	-- enabled = false,
+-- 	opts = {
+-- 		top_down = false,
+-- 	},
+-- }
+
 return {
 	"rcarriga/nvim-notify",
 	-- enabled = false,
 	opts = {
 		top_down = false,
+		max_width = 50,
+		minimum_width = 20,
+		max_height = 8,
+		render = "compact",
+		stages = "static",
+		timeout = 3000, -- ms (default: ~5000)
 	},
 }
